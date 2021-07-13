a7588c924472e4782f87f617d832fc5d21ae60bb
Li Junyang <48000364+junyang10734@users.noreply.github.com>
2021-07-13 15:45:09 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,26 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3>
+          Junyang Li
+        </h3>
+        <a>LeetCode</a>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          About
         </a>
+        <a className="App-link"
+          href="https://www.linkedin.com/in/junyang-li/"
+          target="_blank"
+          rel="noopener noreferrer">LinkedIn</a>
+        <a className="App-link"
+          href="https://github.com/junyang10734"
+          target="_blank"
+          rel="noopener noreferrer">GitHub</a>
       </header>
     </div>
   );
